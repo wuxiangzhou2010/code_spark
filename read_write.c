fa8d328ded2fc421066d6cc8573bbc48d093d9ea
xihajuan2010 <xihajuan2010@gmail.com>
2017-03-15 20:46:33 +0800

    modify read_write.c

--- read_write.c
+++ read_write.c
@@ -1,8 +1,16 @@
+/**
+** usage: 	gcc -g -o read_write read_write.c -Wall
+** 			./read_write
+*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <strings.h>
+#include <unistd.h>
+
+
 #define BUFFER_SIZE (1024)
 char buffer[BUFFER_SIZE];
 
@@ -11,11 +19,16 @@ char *filename2 = "test2.txt";
 int length;
 int main(int argc, char *argv[])
 {
-	int fd = open(filename, O_RDWR);
-	int fd2 = open(filename2, O_RDWR|O_TRUNC);
-	if(fd) /* file open ok */
+	int fd = open(filename, O_RDWR);  /* open for read and write*/
+	int fd2 = open(filename2, O_RDWR|O_CREAT); /* open for read and write, if not exist create it*/
+	if(fd2 < 0)
 	{
-		while(length = read(fd, (void*)buffer, BUFFER_SIZE)) /* get all content, 12 byte each time */
+		printf("open %s Failed\n", filename2);
+	}
+	if(fd > 0) /* file open ok */
+	{
+		/* On success, the number of bytes read is returned,  On error, -1 is returned */
+		while((length = read(fd, (void*)buffer, BUFFER_SIZE))) 
 		{
 			printf("%s",(char *)buffer );
 			write(fd2, (void*)buffer, length == BUFFER_SIZE?BUFFER_SIZE:length);
